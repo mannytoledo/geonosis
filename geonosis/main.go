7d88639cc20558d597e917b6a05056c7ba22178e
Manny Toledo <mtoledo@adobe.com>
2015-06-20 14:55:41 -0700

    new: Basic static page serving

--- geonosis/main.go
+++ geonosis/main.go
@@ -1,38 +1,56 @@
 package main
 
 import (
-  "net/http"
-
-  "github.com/labstack/echo"
-  mw "github.com/labstack/echo/middleware"
+	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
+	"html/template"
+	"io"
+	"net/http"
 )
+
+type (
+	// Template provides HTML template rendering
+	Template struct {
+		templates *template.Template
+	}
+
+	// user struct {
+	// 	ID   string `json:"id"`
+	// 	Name string `json:"name"`
+	// }
+)
+
+func (t *Template) Render(w io.Writer, name string, data interface{}) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 // Handler
 func index(c *echo.Context) error {
-  return c.String(http.StatusOK, "Hello, World!\n")
+	return c.Render(http.StatusOK, "welcome", "Joe")
+	// return c.String(http.StatusOK, "Hello, World!\n")
 }
 
 func deploymentGet(c *echo.Context) error {
-  return c.String(http.StatusOK, "Deployment GET\n")
+	return c.String(http.StatusOK, "Deployment GET\n")
 }
 
 func deploymentPost(c *echo.Context) error {
-  return c.String(http.StatusOK, "Deployment POST\n")
+	return c.String(http.StatusOK, "Deployment POST\n")
 }
 
 func main() {
-  // Echo instance
-  e := echo.New()
+	// Echo instance
+	e := echo.New()
 
-  // Middleware
-  e.Use(mw.Logger())
-  e.Use(mw.Recover())
+	// Middleware
+	e.Use(mw.Logger())
+	e.Use(mw.Recover())
+	e.Index("public/index.html")
 
-  // Routes
-  e.Get("/", index)
-  e.Get("/v1/deployment", deploymentGet)
-  e.Post("/v1/deployment", deploymentPost)
+	// Routes
+	e.Get("/v1/deployment", deploymentGet)
+	e.Post("/v1/deployment", deploymentPost)
 
-  // Start server
-  e.Run(":1323")
+	// Start server
+	e.Run(":1323")
 }
