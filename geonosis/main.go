a514b6d1521814b796aa3119f17cf487c97a7f50
Chris Henry <henry.christopher@gmail.com>
2015-06-20 13:36:35 -0700

    Initial router

---
+++ geonosis/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/labstack/echo"
+  mw "github.com/labstack/echo/middleware"
+)
+
+// Handler
+func hello(c *echo.Context) error {
+  return c.String(http.StatusOK, "Hello, World!\n")
+}
+
+func deploymentGet(c *echo.Context) error {
+  return c.String(http.StatusOK, "Deployment GET\n")
+}
+
+func deploymentPost(c *echo.Context) error {
+  return c.String(http.StatusOK, "Deployment POST\n")
+}
+
+func main() {
+  // Echo instance
+  e := echo.New()
+
+  // Middleware
+  e.Use(mw.Logger())
+  e.Use(mw.Recover())
+
+  // Routes
+  e.Get("/", hello)
+  e.Get("/v1/deployment", deploymentGet)
+  e.Post("/v1/deployment", deploymentPost)
+
+  // Start server
+  e.Run(":1323")
+}
